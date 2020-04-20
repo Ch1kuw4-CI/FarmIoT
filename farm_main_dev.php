141714ca0ee930b7395cb9bbfbcd5bcc3561f615
Ch1kuw4 <itou.shou@corporate.co.jp>
2020-04-20 11:35:08 +0900

    cssクラス追加

--- farm_main_dev.php
+++ farm_main_dev.php
@@ -606,7 +606,7 @@ if (file_exists("images/" . $camera_id . "/" . $dateStr . "/" . $dateStr . "_" .
         ?>
           <td width="10%" algin="center" style="text-align:center;">
             <?php echo substr($timeStr, 0, 2); ?>:<?php echo sprintf('%02d', $min + $i); ?><br />
-            <img src="<?php echo $subImg; ?>" border=1 style="cursor:pointer;margin-left:auto;margin-right:auto;" onClick="viewImage('<?php echo $hh . $d_min . "00"; ?>');">
+            <img class="miniImg" src="<?php echo $subImg; ?>" border=1 style="cursor:pointer;margin-left:auto;margin-right:auto;" onClick="viewImage('<?php echo $hh . $d_min . "00"; ?>');">
           </td>
         <?php } ?>
       </tr>
