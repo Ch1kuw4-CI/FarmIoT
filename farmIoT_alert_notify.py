182d3e9870462333a68dcf723d9d5cf04220ca22
Ch1kuw4 <itou.shou@corporate.co.jp>
2020-05-12 09:41:30 +0900

    LINEトークンをDBから取得するよう修正

--- farmIoT_alert_notify.py
+++ farmIoT_alert_notify.py
@@ -7,15 +7,96 @@ import requests
 import mysql.connector
 
 
+# -----データベースの情報を格納する定数-----
+COMMON_DB_USER = "root"  # 共通DBのユーザ名
+COMMON_DB_PASS = "pm#corporate1"  # 共通DBのパスワード
+COMMON_DB_HOST = "localhost"  # 共通DBのホスト名
+COMMON_DB_NAME = "common_db"  # 共通DBのDB名
+
+PJ_DB_USER = "root"  # プロジェクトで使用するDBのユーザ名
+PJ_DB_PASS = "pm#corporate1"  # プロジェクトで使用するDBのパスワード
+PJ_DB_HOST = "localhost"  # プロジェクトで使用するDBのホスト名
+PJ_DB_NAME = "FARMM_IoT"  # プロジェクトで使用するDB名
+# ---------------------------------------
+
+# -----グローバル変数群-----
+pj_name = "kurozemu"  # プロジェクト名
+limit_tbl_item = None  # 規定値テーブルの項目名
+current_value = None  # 既定値テーブルの現在値
+smtp_addr = None  # 送信元アドレス
+smtp = None  # 送信元接続情報
+line_token = ""  # LINEトークン
+common_pj = None  # 共通データベースへの接続情報を保持する変数
+pj_con = None  # プロジェクトごとのデータベースへの接続情報を保持する変数
+line_message = "<< 農業IoTアラート >>"  # LINE通知のメッセージタイトル
+alert_flg = "OFF"  # LINEアラートが発生したら"ON"
+# --------------------------
+
+# 10分前の時刻を取得
+before_10min = time.time() - 600
+before_10min = datetime.datetime.fromtimestamp(before_10min)
+
+def connect_database_common():
+    """
+    共通データベースにアクセスする処理
+    """
+    global common_pj
+
+    common_pj = mysql.connector.connect(
+        user=COMMON_DB_USER, password=COMMON_DB_PASS, host=COMMON_DB_HOST, database=COMMON_DB_NAME)
+
+
+def connect_database_project():
+    """
+    プロジェクトごとのデータベースにアクセスする処理
+    """
+    global pj_con
+
+    pj_con = mysql.connector.connect(
+        user=PJ_DB_USER, password=PJ_DB_PASS, host=PJ_DB_HOST, database=PJ_DB_NAME)
+
+
+def close_con_connect(con_name, cur_name):
+    """
+    引数で受け取った、データベース接続情報と、カーソルをCloseする処理
+    """
+    con_name.close()
+    cur_name.close()
+
+def get_line_token():
+    """
+    共通データベースからLINEトークンを取得する処理
+    """
+    # グローバル変数に代入するために宣言
+    global line_token
+
+    # データベース接続処理
+    connect_database_common()
+
+    # 共通データベースのカーソルを取得
+    line_cur = common_pj.cursor()
+    line_cur.execute(
+        "SELECT * FROM m_common_token WHERE project_name='" + pj_name + "'")
+
+    for line_row in line_cur.fetchall():
+        # line_id = line_row[0]
+        line_token = line_row[1]
+
+    # 後処理としてクローズ処理を実行する
+    close_con_connect(common_pj, line_cur)
+
+
 def LINE_notify(str_message):
     """
     LINE Notifyの接続処理
     """
+
     url = "https://notify-api.line.me/api/notify"
-#    token = #Here ACCESS-TOKEN input
-#    token = "ObFoG8pLNgIGpm04j7t0abp9wKMAJAuHHp08VFihIOb" #<-TEST用のLINEトークン
-    token = "EujRE1ZyuRxXT8JCpwM2Z6o3zfQ5pGIrjbjTbK2aykp"  # <--くろぜむアラート用のLINEトークン
-    headers = {"Authorization": "Bearer " + token}
+
+    #LINEトークン取得処理
+    get_line_token()
+
+    headers = {"Authorization": "Bearer " + line_token}
     payload = {"message":  str_message}
 
     r = requests.post(url, headers=headers, params=payload)
@@ -38,16 +119,6 @@ def MESSAGE_SET(str_message):
     else:
         pass
     return str_message
-
-
-# 10分前の時刻を取得
-before_10min = time.time() - 600
-before_10min = datetime.datetime.fromtimestamp(before_10min)
-
-# LINE通知のメッセージタイトルを設定
-line_message = "<< 農業IoTアラート >>"
-alert_flg = "OFF"  # LINEアラートが発生したら"ON"になる
-
 
 # 測定値テーブルに接続し直近の測定値を取得
 conn = mysql.connector.connect(
